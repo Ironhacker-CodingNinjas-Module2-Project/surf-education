28628a45ea0b2ff19ff621812b0015211d02aab2
Kati Fe <k-felgitsch@hotmail.com>
2021-10-27 13:57:52 +0100

    Setup File Upload //images do not show up in lit yet

--- routes/surftrick.routes.js
+++ routes/surftrick.routes.js
@@ -3,6 +3,7 @@ const Surftrick = require("../models/Surftrick.model")
 const User = require("../models/User.model")
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
+const fileUploader = require('../config/cloudinary.config');
 
 router.get("/surftrickList", isLoggedIn, (req, res, next) => {
     const userInSession = req.session.user
@@ -25,12 +26,12 @@ router.get("/surftrickList/create", isLoggedIn, (req, res, next) => {
 });
 
 
-router.post("/surftrickList/create", isLoggedIn, (req, res, next) => {
+router.post("/surftrickList/create", fileUploader.single('surftrick-image'), isLoggedIn, (req, res, next) => {
     const { name, image, description, rateOfDifficulty } = req.body;
     const author = req.user._id
     const newSurftrick = {
         name,
-        image,
+        imageURL: req.file.path,
         description,
         rateOfDifficulty,
         author
@@ -48,7 +49,6 @@ router.post("/surftrickList/create", isLoggedIn, (req, res, next) => {
 
 
 router.get("/surftrickList/:surftrickId", isLoggedIn, (req, res, next) => {
-    // const userInSession = req.session.user
     Surftrick.findById(req.params.surftrickId)
         .populate("author")
         .then((surftricksFromDB) => {
@@ -91,15 +91,9 @@ router.post("/surftrickList/:surftrickId/edit", (req, res, next) => {
 })
 
 router.post('/surftrickList/:surftrickId/delete', isLoggedIn, (req, res, next) => {
-    // const userInSession = req.session.user
     const author = req.user._id
     Surftrick.findByIdAndRemove(req.params.surftrickId)
     console.log(req.params.surftrickId, "what is inside")
-    if (!req.user._id) {
-        return res.status(400).render("auth/signup", {
-            errorMessage: "Please provide your username."
-        });
-    } 
     .then(() => {
         res.redirect("/surftrickList")
     })
@@ -107,77 +101,28 @@ router.post('/surftrickList/:surftrickId/delete', isLoggedIn, (req, res, next) =
             console.log("Error deleting surftrick!!", error);
             next(error)
         });
+        
 })
 
-// if (comment.users_id !== req.users.id) {
-//     return res.status(401).json({
-//       error: {
-//         message: `You can only delete your own comments.`
-//       },
-//     });
-//   }
 
 router.post('/surftrickList/:surftrickId/delete', isLoggedIn, (req, res, next) => {
-    const userInSession = req.session.user 
+    const userInSession = req.session.user
     Surftrick.findByIdAndRemove()
-    .populate("author")
-    .then((surftricksFromDB)=>{
-        const data = {
-            surftricksInfo:surftricksFromDB
-        }
-        console.log("dataWWOOOOOORKING",{userInSession, data})
-        console.log("USER IN SESSION", {userInSession})
-        console.log("SURF TRICK FROM DB", {surftricksFromDB})
-        //res.render("surftricks/surftrick-list", {userInSession, data}) 
-    })
-    .catch((error)=>{
-        console.log("Error getting list of surftricks from the DB", error);
-        next(error)
-    });
+        .populate("author")
+        .then((surftricksFromDB) => {
+            const data = {
+                surftricksInfo: surftricksFromDB
+            }
+            console.log("dataWWOOOOOORKING", { userInSession, data })
+            console.log("USER IN SESSION", { userInSession })
+            console.log("SURF TRICK FROM DB", { surftricksFromDB })
+        })
+        .catch((error) => {
+            console.log("Error getting list of surftricks from the DB", error);
+            next(error)
+        });
 })
-    //.then((surftricksFromDB)=>{
-        // const data = {
-        //     surftricksArr:surftricksFromDB
-        // }
 
-        // console.log("data",{userInSession, data})
-    // const currentUserId = req.user._id
-    // const surfTrickData = req.params.surftrickId
-
-//     
-//     Surftrick.find()
-//     .populate("author")
-//     
-
-//      
-   
-    // const surfTricksParams = req.params.surftrick
-    //console.log("IS WOORKING INFO", {surfTrickData, currentUserId})
-    //console.log("PARAMS", surfTricksParams )
-
-    
-
-    // .then((surftrickFromDB)=>{
-    //     const data = {
-    //         surftrickInfo:surftrickFromDB
-    //     }
-    //    console.log("DATA WORKING!!!!", data) 
-
-    //if surf.users_id !== req.users.id
-    // console.log(req.params.surftrickId, "what is inside")
-    // if (!req.user._id) {
-    //     return res.status(400).render("auth/signup", {
-    //         errorMessage: "Please provide your username."
-    //     });
-    // } 
-    // .then(() => {
-    //     res.redirect("/surftrickList")
-    // })
-    // .catch((error) => {
-    //         console.log("Error deleting surftrick!!", error);
-//     //         next(error)
-//      });
-// })
 
 
 
